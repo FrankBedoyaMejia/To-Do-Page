0c37f3ee95e93acfbb9910c6010655eb8211171f
FrankBedoyac4 <franksurenuga@gmail.com>
2023-06-02 17:42:38 -0500

    Created a header an added sass to maka de style

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <ul className="navbar">
+        <li>User Nam</li>
+        <li>Log In/Out</li>
+      </ul>
+    </header>
+  );
+};
