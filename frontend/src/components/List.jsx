d090531b011392b725db4e8003e06282f9b13f2d
FrankBedoyac4 <franksurenuga@gmail.com>
2023-06-03 12:15:49 -0500

    Makin up a style and added the input to write TO DO

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -4,6 +4,10 @@ export const List = () => {
   return (
     <main className="list">
       <h1>TO DO LIST</h1>
+      <div className="list__input">
+        <input type="text" />
+        <button>Create</button>
+      </div>
       <section className="list__background">
         <Card_Todo />
       </section>
